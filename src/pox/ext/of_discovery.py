aceeee17bbd2232aba30b00e0cadff25843523a5
Alessandro Canessa <a.canessa@nextworks.it>
2013-01-11 16:17:43 +0100

    Added first version for message exchanging between discovery and topologymgr

--- src/pox/ext/of_discovery.py
+++ src/pox/ext/of_discovery.py
@@ -28,71 +28,85 @@ It's possible that some of this should be abstracted out into a generic
 Discovery module, or a Discovery superclass.
 """
 
-from pox.lib.revent               import *
-from pox.lib.recoco               import Timer
-from pox.lib.packet.ethernet      import LLDP_MULTICAST, NDP_MULTICAST
-from pox.lib.packet.ethernet      import ethernet
-from pox.lib.packet.lldp          import lldp, chassis_id, port_id, end_tlv
-from pox.lib.packet.lldp          import ttl, system_description
+from pox.lib.revent                import *
+from pox.lib.recoco                import Timer
+from pox.lib.packet.ethernet       import LLDP_MULTICAST, NDP_MULTICAST
+from pox.lib.packet.ethernet       import ethernet
+from pox.lib.packet.lldp           import lldp, chassis_id, port_id, end_tlv
+from pox.lib.packet.lldp           import ttl, system_description
+from pox.lib.util                  import dpidToStr
+from pox.core                      import core
+from pox.messenger.messenger       import *
+from collections                   import *
+
 import pox.openflow.libopenflow_01 as of
-from pox.lib.util                 import dpidToStr
-from pox.core import core
-from pox.messenger.messenger import *
+import pox.openflow.discovery      as discovery
 
 import struct
 import array
 import socket
 import time
 import copy
-from collections import *
-import pox.openflow.discovery as discovery
+import pickle
+import entities
 
 log = core.getLogger()
 
 class Discovery_sample (discovery.Discovery):
 
   def getTopology(self):
-    return self._dps
+    self.topology_ex = core.components['topology']
+    return self.topology_ex.serialize()
 
 class MessengerHandler (object):
   def __init__ (self, targetName):
     core.messenger.addListener(MessageReceived, self._handle_global_MessageReceived, weak=True)
     self._targetName = targetName
+    self._commands   = ["get_topology",
+                        "set_topology"]
 
   def _handle_global_MessageReceived (self, event, msg):
     try:
-      b = core.openflow_discovery.getTopology()
-      print(type(b))
-      print(b)
-      for i in b:
-        print("HERE")
-        print i
-      n = msg['start']
-      if n == self._targetName:
+      command = None
+      # XXX FIXME: Modify the received message (now it is a string)
+      log.debug("Received the following msg: '%s'" % str(msg))
+      for i in msg.values():
+          if (i.encode('utf-8') in self._commands):
+              command = i.encode('utf-8')
+              break
+      if command in self._commands:
+        log.debug("Received a message containing a command to be executed...")
         event.con.read()
         event.claim()
         event.con.addListener(MessageReceived, self._handle_MessageReceived, weak=True)
         print self._targetName, "- started conversation with", event.con
       else:
-        print self._targetName, "- ignoring", n
+        print self._targetName, "- ignoring", command
     except Exception, e:
       print(e)
       pass
 
   def _handle_MessageReceived (self, event, msg):
-    if event.con.isReadable():
-      r = event.con.read()
-      print self._targetName, "-",r
-      if type(r) is dict and r.get("end",False):
-        print self._targetName, "- GOODBYE!"
-        event.con.close()
-      if type(r) is dict and "echo" in r:
-        event.con.send({"echo":r["echo"]})
+    try:
+      topology_dict = core.openflow_discovery.getTopology()
+      topology_obj  = entities.Topology()
+      for i in topology_dict:
+        topology_obj.add_entity(topology_dict[i])
+      topology_2sent = topology_obj.serialize()
+      event.con.send(pickle.dumps(topology_2sent))
+      log.debug("Sent the following response: '%s'" % topology_2sent)
 
-    else:
-      print self._targetName, "- conversation finished"
+      if event.con.isReadable():
+        r = event.con.read()
+        log.debug("%s - %s" % (str(self._targetName),
+                               str(r)))
+        if type(r) is dict and r.get("end",False):
+          log.debug("%s - BYE"  % str(self._targetName))
+          event.con.close()
+    except Exception, e:
+        log.error("Cannot send the response ('%s')" % str(e))
 
-examples = {}
+options = {}
 
 def launch (shell = False, name = "topology"):
     if shell is False:
@@ -100,7 +114,7 @@ def launch (shell = False, name = "topology"):
     else:
         log.debug("Component can interact with an external shell \
                   to extract and/or populated topology DB")
-        examples[name] = MessengerHandler(name)
+        options[name] = MessengerHandler(name)
 
     log.debug("Launching of_discovery component...")
     core.registerNew(Discovery_sample, "Discovery_sample")
