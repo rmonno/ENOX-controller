83cec9d85b0b38a701c6aadb3ec0892725b8d421
Roberto Monno <a.canessa@nextworks.it>
2013-02-12 17:54:40 +0100

    fixed packaging

---
+++ src/libs/__init__.py
@@ -0,0 +1,6 @@
+# export nextworks libreries as a package
+#
+from topology_ofc_manager import TopologyOFCManager
+from connections import Server, message_send, msg_receive
+from fpce_dm import FPCE
+from pce_conn import PCE_Client
