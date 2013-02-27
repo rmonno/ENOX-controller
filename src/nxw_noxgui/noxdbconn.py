8d96382df4dc62d76ec31d06231b5fffb8bb1511
Roberto Monno <a.canessa@nextworks.it>
2013-02-27 17:22:27 +0100

    Fibre NOX-GUI layout

---
+++ src/nxw_noxgui/noxdbconn.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# roberto monno r.monno@nextworks.it
+
+""" Nox DB Connector """
+
+import libs as nxw_utils
+
+
+class NoxDBConnector(nxw_utils.TopologyOFCManager):
+    """ Nox DB Connector object """
+
+    def __init__(self, host, user, pswd, db, logger):
+        nxw_utils.TopologyOFCManager.__init__(self, host, user,
+                                              pswd, db, logger)
+        self.__log = logger
+
+    def retry_switch(self):
+        try:
+            self.open_transaction()
+            return [1, 2, 3, 4, 5]
+
+        except DBException as exe:
+            self.__log(str(exe))
+
+        finally:
+            self.close()
