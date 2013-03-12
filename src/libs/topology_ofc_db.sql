ce5751e3e6e7a2c131ee4ea14d9388a3b260b96e
Roberto Monno <a.canessa@nextworks.it>
2013-03-12 17:19:47 +0100

    Rearranged sql schema

--- src/libs/topology_ofc_db.sql
+++ src/libs/topology_ofc_db.sql
@@ -20,27 +20,13 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS topology_ofc_db;
 GRANT ALL ON topology_ofc_db.* TO 'topology_user'@'%' IDENTIFIED BY 'topology_pwd';
+
 USE topology_ofc_db;
--- --------------------------------------------------------
-
---
--- Table structure for table `cports_bandwidth`
---
-
-CREATE TABLE IF NOT EXISTS `cports_bandwidth` (
-  `dpid` bigint(20) unsigned NOT NULL COMMENT 'datapath identifier',
-  `port_no` smallint(8) unsigned NOT NULL COMMENT 'circuit switch port number',
-  `num_bandwidth` smallint(8) unsigned NOT NULL COMMENT 'identifies number of bandwidth array elements',
-  `bandwidth` bigint(20) unsigned DEFAULT NULL COMMENT 'bandwidth value',
-  PRIMARY KEY (`dpid`,`port_no`,`num_bandwidth`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='circuit ports bandwidth info';
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `datapaths`
 --
-
 CREATE TABLE IF NOT EXISTS `datapaths` (
   `id` bigint(20) unsigned NOT NULL COMMENT 'datapath identifier',
   `name` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'datapath name',
@@ -51,78 +37,16 @@ CREATE TABLE IF NOT EXISTS `datapaths` (
   `cports` tinyint(3) unsigned DEFAULT NULL COMMENT 'Number of circuit ports',
   `dID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT 'unique datapath ID',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `dID` (`dID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='datapaths details' AUTO_INCREMENT=91 ;
+  UNIQUE KEY (`dID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='datapaths details';
 
--- --------------------------------------------------------
-
---
--- Table structure for table `flow_entries`
---
-
-CREATE TABLE IF NOT EXISTS `flow_entries` (
-  `flow_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `dpid` bigint(20) unsigned NOT NULL,
-  `table_id` int(8) unsigned NOT NULL,
-  `in_port` int(16) unsigned NOT NULL,
-  `idle_timeout` int(16) unsigned NOT NULL,
-  `hard_timeout` int(16) unsigned NOT NULL,
-  `priority` int(16) unsigned NOT NULL,
-  `action` varchar(18) CHARACTER SET latin1 NOT NULL,
-  `cookie` bigint(64) unsigned NOT NULL,
-  `dl_type` int(16) unsigned NOT NULL,
-  `dl_vlan` int(16) unsigned NOT NULL,
-  `dl_vlan_pcp` int(8) unsigned NOT NULL,
-  `dl_src` varchar(18) CHARACTER SET latin1 NOT NULL,
-  `dl_dst` varchar(18) CHARACTER SET latin1 NOT NULL,
-  `nw_src` varchar(18) CHARACTER SET latin1 NOT NULL,
-  `nw_dst` varchar(18) CHARACTER SET latin1 NOT NULL,
-  `nw_src_n_wild` int(11) NOT NULL,
-  `nw_dst_n_wild` int(11) NOT NULL,
-  `nw_proto` int(8) unsigned NOT NULL,
-  `tp_src` int(16) unsigned NOT NULL,
-  `tp_dst` int(16) unsigned NOT NULL,
-  UNIQUE KEY `flow_id` (`flow_id`),
-  KEY `dpid` (`dpid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hosts`
---
-
-CREATE TABLE IF NOT EXISTS `hosts` (
-  `mac_addr` varchar(18) CHARACTER SET latin1 NOT NULL,
-  `ip_addr` varchar(18) CHARACTER SET latin1 NOT NULL,
-  `in_port` smallint(8) unsigned NOT NULL,
-  `dpid` bigint(20) unsigned NOT NULL,
-  `hostID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`dpid`,`mac_addr`),
-  UNIQUE KEY `id` (`hostID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='hosts info' AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `links`
---
-
-CREATE TABLE IF NOT EXISTS `links` (
-  `src_dpid` bigint(20) unsigned NOT NULL COMMENT 'source datapath identifier',
-  `src_pno` smallint(8) unsigned NOT NULL COMMENT 'source port number',
-  `dst_dpid` bigint(20) unsigned NOT NULL COMMENT 'destination datapath identifier',
-  `dst_pno` smallint(8) unsigned NOT NULL COMMENT 'destination port number',
-  PRIMARY KEY (`src_dpid`,`src_pno`),
-  KEY `links_ibfk_2` (`dst_dpid`,`dst_pno`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='links info';
+ALTER TABLE `datapaths` AUTO_INCREMENT=1;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `ports`
 --
-
 CREATE TABLE IF NOT EXISTS `ports` (
   `datapath_id` bigint(20) unsigned NOT NULL COMMENT 'datapath identifier',
   `port_no` smallint(8) unsigned NOT NULL COMMENT 'port number',
@@ -140,40 +64,123 @@ CREATE TABLE IF NOT EXISTS `ports` (
   `peer_dpath_id` bigint(20) unsigned DEFAULT NULL COMMENT 'discovered peer switching datapath identifier',
   `nodeID` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT 'unique node identifier',
   PRIMARY KEY (`datapath_id`,`port_no`),
-  UNIQUE KEY `nodeID` (`nodeID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='port details' AUTO_INCREMENT=112 ;
+  UNIQUE KEY (`nodeID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='port details';
 
 --
--- Constraints for dumped tables
+-- Constraints for table `ports`
 --
+ALTER TABLE `ports` AUTO_INCREMENT=1;
+
+ALTER TABLE `ports`
+  ADD CONSTRAINT `ports_ibfk_1` FOREIGN KEY (`datapath_id`)
+        REFERENCES `datapaths` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cports_bandwidth`
+--
+CREATE TABLE IF NOT EXISTS `cports_bandwidth` (
+  `dpid` bigint(20) unsigned NOT NULL COMMENT 'datapath identifier',
+  `port_no` smallint(8) unsigned NOT NULL COMMENT 'circuit switch port number',
+  `num_bandwidth` smallint(8) unsigned NOT NULL COMMENT 'identifies number of bandwidth array elements',
+  `bandwidth` bigint(20) unsigned DEFAULT NULL COMMENT 'bandwidth value',
+  PRIMARY KEY (`dpid`,`port_no`,`num_bandwidth`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='circuit ports bandwidth info';
 
 --
 -- Constraints for table `cports_bandwidth`
 --
 ALTER TABLE `cports_bandwidth`
-  ADD CONSTRAINT `cports_bandwidth_ibfk_1` FOREIGN KEY (`dpid`, `port_no`) REFERENCES `ports` (`datapath_id`, `port_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `cports_bandwidth_ibfk_1` FOREIGN KEY (`dpid`, `port_no`)
+    REFERENCES `ports` (`datapath_id`, `port_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- --------------------------------------------------------
 
 --
--- Constraints for table `flow_entries`
+-- Table structure for table `hosts`
 --
-ALTER TABLE `flow_entries`
-  ADD CONSTRAINT `flow_entries_ibfk_1` FOREIGN KEY (`dpid`) REFERENCES `datapaths` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+CREATE TABLE IF NOT EXISTS `hosts` (
+  `dpid` bigint(20) unsigned NOT NULL,
+  `mac_addr` varchar(18) CHARACTER SET latin1 NOT NULL,
+  `ip_addr` varchar(18) CHARACTER SET latin1 NOT NULL,
+  `in_port` smallint(8) unsigned NOT NULL,
+  `hostID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`dpid`,`mac_addr`),
+  UNIQUE KEY (`hostID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='hosts info';
 
 --
 -- Constraints for table `hosts`
 --
+ALTER TABLE `hosts` AUTO_INCREMENT=1;
+
 ALTER TABLE `hosts`
-  ADD CONSTRAINT `hosts_ibfk_1` FOREIGN KEY (`dpid`) REFERENCES `datapaths` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `hosts_ibfk_1` FOREIGN KEY (`dpid`)
+    REFERENCES `datapaths` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `links`
+--
+CREATE TABLE IF NOT EXISTS `links` (
+  `src_dpid` bigint(20) unsigned NOT NULL COMMENT 'source datapath identifier',
+  `src_pno` smallint(8) unsigned NOT NULL COMMENT 'source port number',
+  `dst_dpid` bigint(20) unsigned DEFAULT NULL COMMENT 'destination datapath identifier',
+  `dst_pno` smallint(8) unsigned DEFAULT NULL COMMENT 'destination port number',
+  PRIMARY KEY (`src_dpid`,`src_pno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='links info';
 
 --
 -- Constraints for table `links`
 --
 ALTER TABLE `links`
-  ADD CONSTRAINT `links_ibfk_2` FOREIGN KEY (`dst_dpid`, `dst_pno`) REFERENCES `ports` (`datapath_id`, `port_no`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `links_ibfk_1` FOREIGN KEY (`src_dpid`, `src_pno`) REFERENCES `ports` (`datapath_id`, `port_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `links_ibfk_1` FOREIGN KEY (`src_dpid`, `src_pno`)
+    REFERENCES `ports` (`datapath_id`, `port_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `links`
+  ADD CONSTRAINT `links_ibfk_2` FOREIGN KEY (`dst_dpid`, `dst_pno`)
+    REFERENCES `ports` (`datapath_id`, `port_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- --------------------------------------------------------
 
 --
--- Constraints for table `ports`
+-- Table structure for table `flow_entries`
 --
-ALTER TABLE `ports`
-  ADD CONSTRAINT `ports_ibfk_1` FOREIGN KEY (`datapath_id`) REFERENCES `datapaths` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+CREATE TABLE IF NOT EXISTS `flow_entries` (
+  `flow_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `dpid` bigint(20) unsigned NOT NULL,
+  `table_id` int(8) unsigned DEFAULT NULL,
+  `in_port` int(16) unsigned DEFAULT NULL,
+  `idle_timeout` int(16) unsigned DEFAULT NULL,
+  `hard_timeout` int(16) unsigned DEFAULT NULL,
+  `priority` int(16) unsigned DEFAULT NULL,
+  `action` varchar(18) CHARACTER SET latin1 NOT NULL,
+  `cookie` bigint(64) unsigned DEFAULT NULL,
+  `dl_type` int(16) unsigned DEFAULT NULL,
+  `dl_vlan` int(16) unsigned DEFAULT NULL,
+  `dl_vlan_pcp` int(8) unsigned DEFAULT NULL,
+  `dl_src` varchar(18) CHARACTER SET latin1 DEFAULT NULL,
+  `dl_dst` varchar(18) CHARACTER SET latin1 DEFAULT NULL,
+  `nw_src` varchar(18) CHARACTER SET latin1 DEFAULT NULL,
+  `nw_dst` varchar(18) CHARACTER SET latin1 DEFAULT NULL,
+  `nw_src_n_wild` int(11) DEFAULT NULL,
+  `nw_dst_n_wild` int(11) DEFAULT NULL,
+  `nw_proto` int(8) unsigned DEFAULT NULL,
+  `tp_src` int(16) unsigned DEFAULT NULL,
+  `tp_dst` int(16) unsigned DEFAULT NULL,
+  PRIMARY KEY (`flow_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Constraints for table `flow_entries`
+--
+ALTER TABLE `flow_entries` AUTO_INCREMENT=1;
+
+ALTER TABLE `flow_entries`
+  ADD CONSTRAINT `flow_entries_ibfk_1` FOREIGN KEY (`dpid`)
+    REFERENCES `datapaths` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- --------------------------------------------------------
