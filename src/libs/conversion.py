c165705268c5a236cb8aa4e8008dfbace84b23f9
Roberto Monno <a.canessa@nextworks.it>
2013-03-14 16:17:06 +0100

    Fixed dpid and portno conversioni

--- src/libs/conversion.py
+++ src/libs/conversion.py
@@ -8,6 +8,8 @@
 def indextoUpperLower(index):
     return (((index & 0xff00) >> 8),(index & 0x00ff))
 
+def indexfromUpperLower(up, low):
+    return (((int(up) & 0x00ff) << 8) | (int(low) & 0x00ff))
 
 def createNodeIPv4(datapath_index, port_index):
     (d_up, d_low) = indextoUpperLower(datapath_index)
