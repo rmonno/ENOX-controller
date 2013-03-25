c67ca263d4d269f4874be98983a1fb516ad22ad9
Alessandro Canessa <a.canessa@nextworks.it>
2013-03-25 11:27:07 +0100

    Modified timeout values for discovery application

--- src/nox-classic/src/nox/netapps/discovery/discovery.py
+++ src/nox-classic/src/nox/netapps/discovery/discovery.py
@@ -1,17 +1,17 @@
 # Copyright 2008 (C) Nicira, Inc.
-# 
+#
 # This file is part of NOX.
-# 
+#
 # NOX is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # NOX is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with NOX.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -27,9 +27,9 @@ from nox.netapps.user_event_log.pyuser_event_log import pyuser_event_log,LogLeve
 from nox.lib.packet.packet_utils      import array_to_octstr
 from nox.lib.packet.packet_utils      import longlong_to_octstr
 from nox.lib.packet.ethernet          import LLDP_MULTICAST, NDP_MULTICAST
-from nox.lib.packet.ethernet          import ethernet  
+from nox.lib.packet.ethernet          import ethernet
 from nox.lib.packet.lldp              import lldp, chassis_id, port_id, end_tlv
-from nox.lib.packet.lldp              import ttl 
+from nox.lib.packet.lldp              import ttl
 from nox.lib.netinet.netinet          import create_datapathid_from_host
 from nox.lib.openflow                 import OFPP_LOCAL
 
@@ -41,27 +41,27 @@ import time
 import copy
 
 LLDP_TTL             = 120 # currently ignored
-LLDP_SEND_PERIOD     = .10 
-TIMEOUT_CHECK_PERIOD = 5.
-LINK_TIMEOUT         = 10.
+LLDP_SEND_PERIOD     = 1.
+TIMEOUT_CHECK_PERIOD = 10.
+LINK_TIMEOUT         = 20.
 
 lg = logging.getLogger('discovery')
 
-# ---------------------------------------------------------------------- 
+# ----------------------------------------------------------------------
 #  Utility function to create an lldp packet given a chassid
-# 
+#
 #  Create lldp packet using the least significant 48 bits of dpid for
-#  chassis ID 
-# 
-# ---------------------------------------------------------------------- 
-  
-def create_discovery_packet(dpid, portno, ttl_):    
+#  chassis ID
+#
+# ----------------------------------------------------------------------
+
+def create_discovery_packet(dpid, portno, ttl_):
 
     # Create lldp packet
     discovery_packet = lldp()
     cid = chassis_id()
 
-    # nbo 
+    # nbo
     cid.fill(4, array.array('B', struct.pack('!Q',dpid))[2:8])
     discovery_packet.add_tlv(cid)
 
@@ -112,13 +112,13 @@ def create_discovery_packet(dpid, portno, ttl_):
 # <br>
 # Each LLDP packet contains the sending port and datapath ID.  The
 # datapath is currently encoded as a 48bit MAC in the chassis ID TLV,
-# hence the lower 16bits are always 0. 
+# hence the lower 16bits are always 0.
 #
 # <b>Shortcomings:</b>
 #
 # The fundamental problem with a centralized approach to topology
 # discovery is that all ports must be scanned linearly which greatly
-# reduces response time. 
+# reduces response time.
 #
 # This should really be implemented on the switch
 
@@ -155,7 +155,7 @@ class discovery(Component):
         self.register_for_datapath_join ( lambda dp,stats : discovery.dp_join(self, dp, stats) )
         self.register_for_datapath_leave( lambda dp       : discovery.dp_leave(self, dp) )
         self.register_for_port_status( lambda dp, reason, port : discovery.port_status_change(self, dp, reason, port) )
-        # register handler for all LLDP packets 
+        # register handler for all LLDP packets
         match = {DL_DST : array_to_octstr(array.array('B',NDP_MULTICAST)),\
                   DL_TYPE: ethernet.LLDP_TYPE}
         self.register_for_packet_match(lambda
@@ -169,12 +169,12 @@ class discovery(Component):
         return str(discovery)
 
     # --
-    # On datapath join, create a new LLDP packet per port 
+    # On datapath join, create a new LLDP packet per port
     # --
 
-    def dp_join(self, dp, stats): 
+    def dp_join(self, dp, stats):
         self.dps[dp] = stats
-  
+
         self.lldp_packets[dp]  = {}
         for port in stats[PORTS]:
             if port[PORT_NO] == OFPP_LOCAL:
@@ -185,18 +185,18 @@ class discovery(Component):
     # On datapath leave, delete all associated links
     # --
 
-    def dp_leave(self, dp): 
-    
+    def dp_leave(self, dp):
+
         if dp in self.dps:
-            del self.dps[dp]  
+            del self.dps[dp]
         if dp in self.lldp_packets:
-            del self.lldp_packets[dp]  
-    
+            del self.lldp_packets[dp]
+
         deleteme = []
         for linktuple in self.adjacency_list:
-            if linktuple[0] == dp or linktuple[2] == dp: 
+            if linktuple[0] == dp or linktuple[2] == dp:
                 deleteme.append(linktuple)
-    
+
         self.delete_links(deleteme)
 
 
@@ -232,6 +232,11 @@ class discovery(Component):
 
       deleteme = []
       for linktuple in self.adjacency_list:
+          print("AAAAAAAAA")
+          print(curtime)
+          print(self.adjacency_list[linktuple])
+          print(LINK_TIMEOUT)
+          print("BBBBBBBBB")
           if (curtime - self.adjacency_list[linktuple]) > LINK_TIMEOUT:
               deleteme.append(linktuple)
               lg.warn('link timeout ('+longlong_to_octstr(linktuple[0])+" p:"\
@@ -241,20 +246,20 @@ class discovery(Component):
 
       self.delete_links(deleteme)
 
-    # ---------------------------------------------------------------------- 
+    # ----------------------------------------------------------------------
     #  Handle incoming lldp packets.  Use to maintain link state
-    # ---------------------------------------------------------------------- 
+    # ----------------------------------------------------------------------
 
     def lldp_input_handler(self, dp_id, inport, ofp_reason, total_frame_len, buffer_id, packet):
-        
+
         assert (packet.type == ethernet.LLDP_TYPE)
-    
+
         if not packet.next:
             lg.error("lldp_input_handler lldp packet could not be parsed")
             return
-    
+
         assert (isinstance(packet.next, lldp))
-    
+
         lldph = packet.next
         if  (len(lldph.tlvs) < 4) or \
             (lldph.tlvs[0].type != lldp.CHASSIS_ID_TLV) or\
@@ -263,23 +268,23 @@ class discovery(Component):
             lg.error("lldp_input_handler invalid lldp packet")
             return
 
-        # parse out chassis id 
+        # parse out chassis id
         if lldph.tlvs[0].subtype != chassis_id.SUB_MAC:
             lg.error("lldp chassis ID subtype is not MAC, ignoring")
             return
         assert(len(lldph.tlvs[0].id) == 6)
         cid = lldph.tlvs[0].id
-    
+
         # convert 48bit cid (in nbo) to longlong
         chassid = cid[5]       | cid[4] << 8  | \
                   cid[3] << 16 | cid[2] << 24 |  \
-                  cid[1] << 32 | cid[0] << 40 
+                  cid[1] << 32 | cid[0] << 40
 
         # if chassid is from a switch we're not connected to, ignore
         if chassid not in self.dps:
             lg.debug('Recieved LLDP packet from unconnected switch')
             return
-    
+
         # grab 16bit port ID from port tlv
         if lldph.tlvs[1].subtype != port_id.SUB_PORT:
             return # not one of ours
@@ -293,54 +298,57 @@ class discovery(Component):
             return
 
         # print 'LLDP packet in from',longlong_to_octstr(chassid),' port',str(portid)
-    
+
         linktuple = (dp_id, inport, chassid, portid)
-    
+
         if linktuple not in self.adjacency_list:
             self.add_link(linktuple)
             lg.warn('new link detected ('+longlong_to_octstr(linktuple[0])+' p:'\
                        +str(linktuple[1]) +' -> '+\
                        longlong_to_octstr(linktuple[2])+\
                        ' p:'+str(linktuple[3])+')')
-    
-    
-        # add to adjaceny list or update timestamp
-        self.adjacency_list[(dp_id, inport, chassid, portid)] = time.time()
 
-    # ---------------------------------------------------------------------- 
+
+        # add to adjaceny list or update timestamp
+        print("1111111111111")
+        print(time.time())
+        self.adjacency_list[(dp_id, inport, chassid, portid)] = time.time()
+        print("1111111111111")
+
+    # ----------------------------------------------------------------------
     # Start LLDP timer which sends an LLDP packet every
     # LLDP_SEND_PEROID.
-    # ---------------------------------------------------------------------- 
+    # ----------------------------------------------------------------------
 
     def start_lldp_timer_thread(self):
 
-        #---------------------------------------------------------------------- 
+        #----------------------------------------------------------------------
         # Generator which iterates over a set of dp's and sends an LLDP packet
-        # out of each port.  
-        #---------------------------------------------------------------------- 
-        
+        # out of each port.
+        #----------------------------------------------------------------------
+
         def send_lldp (packets):
             for dp in packets:
                 # if they've left, ignore
                 if not dp in self.dps:
                     continue
-                try:    
+                try:
                     for port in packets[dp]:
                         #print 'Sending packet out of ',longlong_to_octstr(dp), ' port ',str(port)
                         self.send_openflow_packet(dp, packets[dp][port].tostring(), port)
-                        yield dp 
+                        yield dp
                 except Exception, e:
                     # catch exception while yielding
                     lg.error('Caught exception while yielding'+str(e))
-        
+
         def build_lldp_generator():
-            
+
             def g():
                 try:
                     g.sendfunc.next()
-                except StopIteration, e:    
+                except StopIteration, e:
                     g.sendfunc = send_lldp(copy.deepcopy(self.lldp_packets))
-                except Exception, e:    
+                except Exception, e:
                     lg.error('Caught exception from generator '+str(e))
                     g.sendfunc = send_lldp(copy.deepcopy(self.lldp_packets))
                 self.post_callback(self.lldp_send_period, g)
@@ -356,25 +364,25 @@ class discovery(Component):
                       linktuple[1], linktuple[3], action)
         self.post(e)
 
-    #---------------------------------------------------------------------- 
+    #----------------------------------------------------------------------
     #  Link addition/deletion
-    #---------------------------------------------------------------------- 
+    #----------------------------------------------------------------------
 
-    def add_link(self, linktuple):            
+    def add_link(self, linktuple):
         self.post_link_event(linktuple, Link_event.ADD)
         src_dpid = create_datapathid_from_host(linktuple[0])
         src_port = linktuple[1]
         dst_dpid = create_datapathid_from_host(linktuple[2])
         dst_port = linktuple[3]
         self._bindings.add_link(src_dpid,src_port,dst_dpid,dst_port)
-        self._user_event_log.log("discovery",LogLevel.ALERT, 
+        self._user_event_log.log("discovery",LogLevel.ALERT,
             "Added network link between {sl} and {dl}",
-            set_src_loc = (src_dpid,src_port), 
-            set_dst_loc = (dst_dpid,dst_port)) 
+            set_src_loc = (src_dpid,src_port),
+            set_dst_loc = (dst_dpid,dst_port))
 
     def delete_links(self, deleteme):
 
-        for linktuple in deleteme:                    
+        for linktuple in deleteme:
             del self.adjacency_list[linktuple]
             src_dpid = create_datapathid_from_host(linktuple[0])
             src_port = linktuple[1]
@@ -382,18 +390,18 @@ class discovery(Component):
             dst_port = linktuple[3]
             try:
                 self._bindings.remove_link(src_dpid,src_port,dst_dpid,dst_port)
-                self._user_event_log.log("discovery",LogLevel.ALERT, 
-                  "Removed network link between {sl} and {dl}", 
-                  set_src_loc = (src_dpid,src_port), 
-                  set_dst_loc = (dst_dpid,dst_port)) 
-            except Exception, e:        
+                self._user_event_log.log("discovery",LogLevel.ALERT,
+                  "Removed network link between {sl} and {dl}",
+                  set_src_loc = (src_dpid,src_port),
+                  set_dst_loc = (dst_dpid,dst_port))
+            except Exception, e:
               lg.error('Error removing links from binding storage')
-                
+
             self.post_link_event(linktuple, Link_event.REMOVE)
 
-    #---------------------------------------------------------------------- 
+    #----------------------------------------------------------------------
     #   Public API
-    #---------------------------------------------------------------------- 
+    #----------------------------------------------------------------------
 
     def is_switch_only_port(self, dpid, port):
         """ Returns True if (dpid, port) designates a port that has any
