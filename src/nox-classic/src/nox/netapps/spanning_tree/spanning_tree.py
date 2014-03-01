32085cbf3093cfe4ef74bad448c42c11ca626728
Monno Roberto <r.monno@nextworks.it>
2014-03-01 12:46:34 +0100

    Changed log procedure and refactoring

--- src/nox-classic/src/nox/netapps/spanning_tree/spanning_tree.py
+++ src/nox-classic/src/nox/netapps/spanning_tree/spanning_tree.py
@@ -9,19 +9,18 @@
 #        any one "link". ie. if we were on a broadcast network or there was an
 #        extra switch in the middle
 # ----------------------------------------------------------------------
-
 import array
 import struct
 import time
-from nox.coreapps.pyrt.pycomponent      import CONTINUE, STOP
+
+from nox.coreapps.pyrt.pycomponent import CONTINUE, STOP
 from nox.netapps.bindings_storage.pybindings_storage import pybindings_storage
-from nox.lib.core                   import *
-from nox.lib.util                   import *
-from nox.lib.packet.packet_utils    import longlong_to_octstr
-from nox.lib.packet.ethernet        import ethernet, ETHER_ANY, ETHER_BROADCAST
-from nox.lib.netinet                import *
+from nox.lib.core import *
+from nox.lib.util import *
+from nox.lib.packet.ethernet import ethernet, ETHER_ANY, ETHER_BROADCAST
+from nox.lib.netinet import *
 import nox.lib.openflow as openflow
-from twisted.python                 import log
+from twisted.python import log
 
 # How often should we rebuild the flood ports?
 FLOOD_PORT_UPDATE_INTERVAL = 5
@@ -32,45 +31,46 @@ FLOOD_WAIT_TIME = 10
 # Minimum LLDP packet send period
 MIN_LLDP_SEND_PERIOD = 0.05
 
-class Spanning_Tree(Component):
+stp_logger = logging.getLogger('nox.netapps.spanning_tree.spanning_tree')
 
+class Spanning_Tree(Component):
     def __init__(self, ctxt):
         Component.__init__(self, ctxt)
 
         self.datapaths = {}
-        self.debug = True
         self.ip_bypass = set()
         self.mac_bypass = set()
         self.roots = set()
         self.port_count = 0
-        
+
     def getInterface(self):
         return str(Spanning_Tree)
 
-    def debugPrint(self, text):
-        if (self.debug):
-            print(text)
+    def __info(self, text):
+        stp_logger.info('%s' % (str(text),))
+
+    def __error(self, text):
+        stp_logger.error('%s' % (str(text),))
 
     def install(self):
         # Ensure LLDP queries occur more frequently by default.
         self.update_lldp_send_period()
 
         # Register to learn about datapath join and leave events
-        self.register_for_datapath_join ( self.dp_join )
-        self.register_for_datapath_leave( self.dp_leave )
-        self.register_for_port_status( self.handle_port_status )
-        self.register_for_packet_in( self.handle_packet_in)
+        self.register_for_datapath_join (self.dp_join)
+        self.register_for_datapath_leave(self.dp_leave)
+        self.register_for_port_status(self.handle_port_status)
+        self.register_for_packet_in(self.handle_packet_in)
 
         self.bindings = self.resolve(pybindings_storage)
 
         self.post_callback(1, self.update_spanning_tree)
-        self.debugPrint("Spanning tree installed\n")
+        self.__info("Spanning tree installed!")
 
     def dp_join(self, dp, stats):
-        self.debugPrint("Datapath join: "+longlong_to_octstr(dp)[6:])
+        self.__info("Datapath join: %x" % (dp,))
         if (not self.datapaths.has_key(dp)):
             # Process the port information returned by the switch
-
             # Build a list of ports
             now = time.time()
             ports = {}
@@ -81,29 +81,31 @@ class Spanning_Tree(Component):
                     port['flood'] = False
                     hw_addr = "\0\0" + port[core.HW_ADDR]
                     hw_addr = struct.unpack("!q", hw_addr)[0]
-                    self.ctxt.send_port_mod(dp, port[core.PORT_NO], ethernetaddr(hw_addr),
-                            openflow.OFPPC_NO_FLOOD, openflow.OFPPC_NO_FLOOD)
-
+                    self.ctxt.send_port_mod(dp,
+                                            port[core.PORT_NO],
+                                            ethernetaddr(hw_addr),
+                                            openflow.OFPPC_NO_FLOOD,
+                                            openflow.OFPPC_NO_FLOOD)
             # Record the datapath
             self.datapaths[dp] = ports
             self.port_count += len(ports)
 
             # Update the LLDP send period
             self.update_lldp_send_period()
+
         return CONTINUE
 
     def dp_leave(self, dp):
-
-        self.debugPrint("Datapath leave, "+longlong_to_octstr(dp)[6:])
+        self.__info("Datapath leave: %x" % (dp,))
         if (self.datapaths.has_key(dp)):
             # Decrement port count by # of ports in datapath that is leaving
             self.port_count -= len(self.datapaths[dp])
             del self.datapaths[dp]
+
         return CONTINUE
 
     def update_spanning_tree(self):
-        '''Get the links to update the spanning tree
-        '''
+        '''Get the links to update the spanning tree'''
         self.bindings.get_all_links(self.update_spanning_tree_callback)
         self.post_callback(FLOOD_PORT_UPDATE_INTERVAL, self.update_spanning_tree)
 
@@ -124,8 +126,7 @@ class Spanning_Tree(Component):
 
         @param links list link tuples (src_dpid, src_port, dst_dpid, dst_port)
         '''
-        # Walk through the datapaths and mark all ports 
-        # that are potentially enableable
+        # Walk through the datapaths and mark all ports potentially enabled
         now = time.time()
         for dp in self.datapaths.iterkeys():
             for port_no, port in self.datapaths[dp].iteritems():
@@ -133,8 +134,8 @@ class Spanning_Tree(Component):
                     port['enable'] = True
                 else:
                     port['enable'] = False
-                port['keep'] = False
 
+                port['keep'] = False
 
         # Walk through the links and create a dict based on source port
         my_links = self.build_link_dict(links)
@@ -164,7 +165,7 @@ class Spanning_Tree(Component):
             # Make sure we know the src_dpid
             # This is necessary occasionally during start-up
             if not my_links.has_key(src_dpid):
-                self.debugPrint("Warning: cannot find src_dpid %s in my_links"%longlong_to_octstr(src_dpid)[6:])
+                self.__error("Cannot find src_dpid %x in my_links" % (src_dpid,))
                 continue
 
             # Walk through all dests
@@ -182,6 +183,7 @@ class Spanning_Tree(Component):
                             if speed > best_speed:
                                 best_speed = speed
                                 best_pair = (src_port, dst_port)
+
                         except KeyError:
                             pass
 
@@ -193,6 +195,7 @@ class Spanning_Tree(Component):
                             else:
                                 self.datapaths[src_dpid][src_port]['keep'] = True
                                 self.datapaths[dst_dpid][dst_port]['keep'] = True
+
                         except KeyError:
                             pass
 
@@ -218,8 +221,10 @@ class Spanning_Tree(Component):
                         try:
                             if not self.datapaths[local_dst_dpid][dst_port]['keep']:
                                 self.datapaths[local_dst_dpid][dst_port]['enable'] = False
+
                             if not self.datapaths[local_src_dpid][src_port]['keep']:
                                 self.datapaths[local_src_dpid][src_port]['enable'] = False
+
                         except KeyError:
                             pass
 
@@ -231,13 +236,15 @@ class Spanning_Tree(Component):
             for dpid in next_dpids:
                 try:
                     srcs.remove(dpid)
+
                 except ValueError:
                     pass
+
             srcs.extend(next_dpids)
 
         # Update the list of roots
         self.roots = roots
-        
+
         # Walk through links and enable/disable as appropriate
         for dp in self.datapaths.iterkeys():
             floodports = []
@@ -254,7 +261,7 @@ class Spanning_Tree(Component):
                             msg = 'Enabling'
                             config = 0
 
-                        self.debugPrint("%s port: %s--%d"%(msg, longlong_to_octstr(dp)[6:], port_no))
+                        self.__info("%s port: %x:::%d" % (msg, dp, port_no,))
                         hw_addr = "\0\0" + port[core.HW_ADDR]
                         hw_addr = struct.unpack("!q", hw_addr)[0]
                         self.ctxt.send_port_mod(dp, port[core.PORT_NO], ethernetaddr(hw_addr),
@@ -265,14 +272,11 @@ class Spanning_Tree(Component):
                     else:
                         nonfloodports.append(port_no)
 
-            self.debugPrint("Ports for %s: Flood: %s   Non-flood: %s"%(longlong_to_octstr(dp)[6:], floodports, nonfloodports))
+            self.__info("Ports for %x: Flood: %s Non-flood: %s" % (dp, floodports, nonfloodports,))
 
     def build_link_dict(self, links):
         '''Build a dictionary of links based on source dpid
-
-        Dict is:
-        {src_dpid: {dst_dpid: [(src_port, dst_port), ...]}}
-        '''
+        Dict: {src_dpid: {dst_dpid: [(src_port, dst_port), ...]}}'''
         my_links = {}
         for (src_dpid, src_port, dst_dpid, dst_port) in links:
             # Track the link
@@ -288,15 +292,15 @@ class Spanning_Tree(Component):
                     else:
                         my_links[src_dpid] = {dst_dpid:set()}
                         my_links[src_dpid][dst_dpid].add((src_port, dst_port))
+
             except KeyError:
                 pass
+
         return my_links
 
     def verify_bidir_links(self, links):
         '''Verify that all links are bi-directional
-
-        Delete unidirectional links and disable ports
-        '''
+        Delete unidirectional links and disable ports'''
         srcs_to_delete = []
         for src_dpid in links.keys():
             dsts_to_delete = []
@@ -308,22 +312,22 @@ class Spanning_Tree(Component):
                     try:
                         if (dst_port, src_port) not in links[dst_dpid][src_dpid]:
                             ok = False
+
                     except KeyError:
                         ok = False
 
                     if not ok:
-                        self.debugPrint("WARNING: Unidirectional link detected between %s -- %d   <-->   %s -- %d"%
-                                (longlong_to_octstr(src_dpid)[6:], src_port,
-                                 longlong_to_octstr(dst_dpid)[6:], dst_port))
+                        self.__error("Unidir link detected between %x:::%d <--> %x:::%d" %
+                                     (src_dpid, src_port, dst_dpid, dst_port,))
                         ports_to_delete.append((src_port, dst_port))
                         try:
                             if (src_dpid <= dst_dpid):
                                 self.datapaths[dst_dpid][dst_port]['enable'] = False
                             else:
                                 self.datapaths[src_dpid][src_port]['enable'] = False
+
                         except KeyError:
                             self.datapaths[src_dpid][src_port]['enable'] = False
-
 
                 # Delete the ports and work out if we need to delete the dst_dpid
                 for ports in ports_to_delete:
@@ -343,15 +347,11 @@ class Spanning_Tree(Component):
 
     def handle_port_status(self, dpid, reason, port):
         '''Port_status_event handler
-
         Handles port stats events, such as adding and deleting ports
 
         dpid - Datapath ID of port
-
         reason - what event occured
-
-        port - port
-        '''
+        port - port'''
         # Work out what sort of event we're processing
         if reason == openflow.OFPPR_ADD:
             if port['port_no'] <= openflow.OFPP_MAX:
@@ -359,10 +359,15 @@ class Spanning_Tree(Component):
                 port['flood'] = False
                 hw_addr = "\0\0" + port[core.HW_ADDR]
                 hw_addr = struct.unpack("!q", hw_addr)[0]
-                self.ctxt.send_port_mod(dp, port[core.PORT_NO], ethernetaddr(hw_addr),
-                        openflow.OFPPC_NO_FLOOD, openflow.OFPPC_NO_FLOOD)
+                self.ctxt.send_port_mod(dp,
+                                        port[core.PORT_NO],
+                                        ethernetaddr(hw_addr),
+                                        openflow.OFPPC_NO_FLOOD,
+                                        openflow.OFPPC_NO_FLOOD)
+
             self.datapaths[dpid][port['port_no']] = port
             self.port_count += 1
+
         elif reason == openflow.OFPPR_DELETE:
             if self.datapaths[dpid].has_key(port['port_no']):
                 self.port_count += 1
@@ -372,26 +377,18 @@ class Spanning_Tree(Component):
 
     def handle_packet_in(self, dpid, inport, reason, len, bufid, packet):
         '''Packet in callback function
-
         Allow packets to be processed by other modules only if 
         the port is a flood port or it's an LLDP packet
 
         dpid - DPID of switch
-
         inport - inport port
-
         reason -
-
         len - length
-
         bufid - buffer ID of packet
-
-        packet - received packet
-        '''
-
+        packet - received packet'''
         if not packet.parsed:
-            log.msg('Ignoring incomplete packet',system='spanning_tree')
-            
+            log.msg('Ignoring incomplete packet', system='spanning_tree')
+
         # Allow LLDP messages to be processed
         if packet.type == ethernet.LLDP_TYPE:
             return CONTINUE
@@ -419,6 +416,7 @@ class Spanning_Tree(Component):
                 return CONTINUE
             else:
                 return STOP
+
         except KeyError:
             return STOP
 
@@ -433,7 +431,6 @@ class Spanning_Tree(Component):
 
     def add_ip_bypass(self, ip):
         '''Add a bypass IP address
-
         Bypass IP addresses should be ignored when conisdering datapath'''
         self.ip_bypass.add(ip)
 
@@ -443,7 +440,6 @@ class Spanning_Tree(Component):
 
     def add_mac_bypass(self, mac):
         '''Add a bypass MAC address
-
         Bypass MAC addresses should be ignored when conisdering datapath'''
         self.mac_bypass.add(mac)
 
